ca21b2083e5b366195a80ff16d7ccc1db5e66063
richrdd <159516192+richrdd@users.noreply.github.com>
2024-06-03 18:52:39 -0700

    Practica #7 03/06/24

--- parcial3/spotify.sql
+++ parcial3/spotify.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 29-05-2024 a las 03:03:52
+-- Tiempo de generación: 04-06-2024 a las 03:51:45
 -- Versión del servidor: 10.1.39-MariaDB
 -- Versión de PHP: 7.3.5
 
@@ -32,7 +32,35 @@ CREATE TABLE `artistas` (
   `id` int(11) NOT NULL,
   `nombre` varchar(30) NOT NULL,
   `descripcion` varchar(300) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `artistas`
+--
+
+INSERT INTO `artistas` (`id`, `nombre`, `descripcion`) VALUES
+(1, 'Taylor Swift', 'Taylor Alison Swift es una cantante, compositora, productora discográfica, directora, actriz y empresaria estadounidense. Criada en Wyomissing, se mudó a Nashville a los 14 años para realizar una carrera de música country.'),
+(2, 'Justin Bieber', 'Justin Drew Bieber es un cantautor canadiense.'),
+(3, 'Travis Scott', 'Jacques Berman Webster II, ​ más conocido por su nombre artístico Travis Scott, es un rapero, compositor y productor musical estadounidense.'),
+(4, 'Kendrick Lamar', 'Kendrick Lamar Duckworth es un rapero, compositor, productor discográfico estadounidense.'),
+(5, 'Dua Lipa', 'Dua Lipa es una cantante, compositora y modelo angloalbanesa.'),
+(6, 'Oscar Maydon', 'Nacido en 1999 en Mexicali, Baja California, Maydon atrajo la atención del sello discográfico Rancho Humilde en 2020 con el tema de su autoría “De La Nació”, notable por una letra ágil y desenfadada.'),
+(7, 'Gabito Ballesteros', 'Gabriel \"Gabito\" Ballesteros Abril es un cantautor y productor discográfico mexicano.'),
+(8, 'Natanael Cano', 'Nathanahel Rubén Cano Monge es un cantante y compositor mexicano dedicado principalmente al género musical corridos tumbados, además de ser acreditado como precursor del mismo.​'),
+(9, 'Junior H', 'Antonio Herrera Pérez, más conocido como \'Junior H\' es un cantante y compositor mexicano. Se especializa en el subgénero de corridos tumbados, popularizando el género con sus primeros álbumes de estudio.​'),
+(10, 'Peso Pluma', 'Hassan Emilio Kabande Laija, conocido artisticamente como Peso Pluma, es un cantante y compositor mexicano. Se especializa en los géneros de corridos tumbados, reguetón y trap latino.​​Su fama internacional comenzó en 2022, después de colaborar con cantantes como Luis R. Conriquez, Natanael Cano y E');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `artistas_canciones`
+--
+
+CREATE TABLE `artistas_canciones` (
+  `id` int(11) NOT NULL,
+  `artista_id` int(11) NOT NULL,
+  `cancion_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -48,7 +76,19 @@ CREATE TABLE `canciones` (
   `fecha` date NOT NULL,
   `activo` tinyint(1) NOT NULL,
   `foto` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `favoritas`
+--
+
+CREATE TABLE `favoritas` (
+  `id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `cancion_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -59,7 +99,21 @@ CREATE TABLE `canciones` (
 CREATE TABLE `generos` (
   `id` int(11) NOT NULL,
   `nombre` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `generos`
+--
+
+INSERT INTO `generos` (`id`, `nombre`) VALUES
+(1, 'kpop'),
+(2, 'jazz'),
+(3, 'pop'),
+(4, 'Musica clasica'),
+(5, 'Blues'),
+(6, 'salsa'),
+(7, 'rock and roll'),
+(8, 'flamenco');
 
 -- --------------------------------------------------------
 
@@ -69,25 +123,47 @@ CREATE TABLE `generos` (
 
 CREATE TABLE `membresias` (
   `id` int(11) NOT NULL,
-  `descripcion` varchar(100) NOT NULL,
+  `desc` varchar(100) NOT NULL,
   `precio` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `membresias`
+--
+
+INSERT INTO `membresias` (`id`, `desc`, `precio`) VALUES
+(1, 'plan Premium Individual', 129),
+(2, ' el plan Premium Duo', 169),
+(3, 'el plan Premium Familiar ', 199);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `playlists`
+-- Estructura de tabla para la tabla `playlist`
 --
 
-CREATE TABLE `playlists` (
+CREATE TABLE `playlist` (
   `id` int(11) NOT NULL,
   `nombre` varchar(200) NOT NULL,
   `usuario_id` int(11) NOT NULL,
   `duracion` time NOT NULL,
   `total_canciones` int(11) NOT NULL,
-  `descripcion` varchar(200) NOT NULL,
+  `desc` varchar(200) NOT NULL,
   `publica` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `playlist_canciones`
+--
+
+CREATE TABLE `playlist_canciones` (
+  `id` int(11) NOT NULL,
+  `playlist_id` int(11) NOT NULL,
+  `cancion_id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -100,7 +176,7 @@ CREATE TABLE `usuarios` (
   `nombre` varchar(100) NOT NULL,
   `correo` varchar(150) NOT NULL,
   `membresia_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Índices para tablas volcadas
@@ -113,11 +189,27 @@ ALTER TABLE `artistas`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `artistas_canciones`
+--
+ALTER TABLE `artistas_canciones`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `artista_id` (`artista_id`),
+  ADD KEY `cancion_id` (`cancion_id`);
+
+--
 -- Indices de la tabla `canciones`
 --
 ALTER TABLE `canciones`
   ADD PRIMARY KEY (`id`),
   ADD KEY `genero_id` (`genero_id`);
+
+--
+-- Indices de la tabla `favoritas`
+--
+ALTER TABLE `favoritas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario_id` (`usuario_id`),
+  ADD KEY `cancion_id` (`cancion_id`);
 
 --
 -- Indices de la tabla `generos`
@@ -132,10 +224,19 @@ ALTER TABLE `membresias`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `playlists`
+-- Indices de la tabla `playlist`
 --
-ALTER TABLE `playlists`
+ALTER TABLE `playlist`
   ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+--
+-- Indices de la tabla `playlist_canciones`
+--
+ALTER TABLE `playlist_canciones`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `playlist_id` (`playlist_id`),
+  ADD KEY `cancion_id` (`cancion_id`),
   ADD KEY `usuario_id` (`usuario_id`);
 
 --
@@ -144,6 +245,34 @@ ALTER TABLE `playlists`
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id`),
   ADD KEY `membresia_id` (`membresia_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `artistas`
+--
+ALTER TABLE `artistas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT de la tabla `artistas_canciones`
+--
+ALTER TABLE `artistas_canciones`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `favoritas`
+--
+ALTER TABLE `favoritas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `playlist_canciones`
+--
+ALTER TABLE `playlist_canciones`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
